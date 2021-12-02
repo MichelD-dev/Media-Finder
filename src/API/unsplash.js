827e1dacebab2e0068ece6bddb6a7fc1a9ad9350
michelD-dev <delaunaymichel@hotmail.fr>
2021-12-02 09:42:50 +0100

    fix params Unsplash

--- src/API/unsplash.js
+++ src/API/unsplash.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 export default axios.create({
   baseURL: 'https://api.unsplash.com/search/photos',
-  headers: {
-    Authorization: process.env.REACT_APP_API_AUTH,
+  params: {
+    client_id: process.env.REACT_APP_API_AUTH,
+    per_page: 20,
   },
 })
